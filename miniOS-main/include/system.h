87085b0f8441b6dcf73a13d0c79631352e4c1362
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-09 12:54:33 +0900

    start

--- miniOS-main/include/system.h
+++ miniOS-main/include/system.h
@@ -3,3 +3,8 @@
 //SSU struct task_struct {
 
 void minisystem();
+void add(char *, char *);
+void Fork();
+void IPC();
+void Est_PI();
+void RRS();
--- miniOS-main/kernel/kernel.c
+++ miniOS-main/kernel/kernel.c
@@ -7,90 +7,56 @@
 #include "system.h"
 void print_minios(char* str);
 
-#define MAX_PROCESSES 10
-
-typedef enum {
-    PROCESS_NEW,
-    PROCESS_READY,
-    PROCESS_RUNNING,
-    PROCESS_WAITING,
-    PROCESS_TERMINATED
-} ProcessState;
-
-typedef struct {
-    int pid;
-    ProcessState state;
-    char process_name[256];
-} ProcessControlBlock;
-
-ProcessControlBlock processTable[MAX_PROCESSES];
-int currentProcess = -1;
-
-void initializeProcessTable() {
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        processTable[i].pid = -1;
-        processTable[i].state = PROCESS_NEW;
-    }
-}
-
-int createProcess(char *process_name) {
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        if (processTable[i].state == PROCESS_NEW || processTable[i].state == PROCESS_TERMINATED) {
-            processTable[i].pid = i;
-            strcpy(processTable[i].process_name, process_name);
-            processTable[i].state = PROCESS_READY;
-            return i;
-        }
-    }
-    return -1;
-}
-
-void terminateProcess(int pid) {
-    if (pid >= 0 && pid < MAX_PROCESSES) {
-        processTable[pid].state = PROCESS_TERMINATED;
-        printf("[MiniOS SSU] Process %d terminated.\n", pid);
-    }
-}
-
 int main() {
-    initializeProcessTable();
     print_minios("[MiniOS SSU] Hello, World!");
-
     char *input;
 
     while(1) {
+        // readline을 사용하여 입력 받기
         input = readline("커맨드를 입력하세요(종료:exit) : ");
 
         if (strcmp(input, "exit") == 0) {
             break;
         }
 
-        if (strcmp(input, "minisystem") == 0) {
+        else if (strcmp(input, "minisystem") == 0){
             minisystem();
         }
-        else if (strcmp(input, "create") == 0) {
-            int pid = createProcess("Sample Process");
-            if (pid != -1) {
-                printf("Process created with PID: %d\n", pid);
-            } else {
-                printf("Failed to create process.\n");
-            }
-        }
-        else if (strncmp(input, "kill ", 5) == 0) {
-            int pid = atoi(input + 5);
-            terminateProcess(pid);
-        }
-        else {
-            system(input);
-        }
+		
+		else if (strcmp(input, "add") == 0) {
+			char *a, *b;
+			a = readline("첫 번째 값 : ");
+			b = readline("두 번째 값 : ");
+			
+			add(a, b);
+		}
+		
+		else if (strcmp(input, "fork") == 0) {
+			Fork();
+		}
+
+        else if (strcmp(input, "ipc") == 0) {
+			IPC();
+		}
+
+        else if (strcmp(input, "est_pi") == 0) {
+			Est_PI();
+		}
+
+        else if (strcmp(input, "rrs") == 0) {
+			RRS();
+		}
+
+        else system(input);
     }
 
+    // 메모리 해제
     free(input);
     print_minios("[MiniOS SSU] MiniOS Shutdown........");
 
-    return 1;
+    return(1);
 }
 
 void print_minios(char* str) {
-    printf("%s\n", str);
+        printf("%s\n", str);
 }
